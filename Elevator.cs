91d9d4267fd323763b482480c00992f498fa7fc4
siatruna <sivagamip@outlook.com>
2021-05-07 00:20:40 -0700

    Add files via upload

---
+++ Elevator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevatorControl
+{
+    public class Elevator
+    {
+        //Floors available for the elevator to travel. 
+        private int Floor;
+
+        //The current floor the elevator is present. Defaults to zero. 
+        private int CurrentFloor;
+
+        //The direction in which the elevator needs to go. By default, value is Rest. 
+        private enum ElevatorDirection
+        {
+            Up,
+            Down,
+            Wait,
+            Rest
+        }
+
+        //Constructor
+        public Elevator()
+        {
+
+        }
+
+        //call to go up.
+        public void GoUp()
+        {
+            this.CurrentFloor += 1;
+        }
+
+        //call to go down the elevator. 
+        public void GoDown()
+        {
+            this.CurrentFloor -= 1;
+        }
+
+        //Requesting an elevator from any floor to any floor. 
+        public void RequestElevator(int floorOfRequest, int destinationFloor)
+        {
+            if (destinationFloor > floorOfRequest)
+            {
+                while (floorOfRequest != CurrentFloor)
+                {
+                    this.GoDown();
+                }
+            }
+            else
+            {
+                while (floorOfRequest != CurrentFloor)
+                {
+                    this.GoUp();
+                }
+            }
+
+        }
+
+        // person pushes button and this method is called. 
+        public void ElevatorTravel(int destinationFloor)
+        {
+            //using above logic, make sure to check the current floor and if 
+            //current floor iss greater than destination floor, then move the elevator down else up. 
+        }
+
+
+
+    }
+}
---
+++ Floors.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElevatorControl
+{
+    public class Floors
+    {
+        // total number of floors the elevator is supposed to travel. 
+        private enum FloorsCatered
+        {
+
+        }
+
+
+        //As a user enters and clicks a floor, add in to the Array list.
+        //As you add to the array list, keep the list sorted. 
+        private ArrayList FloorsTravelled;
+
+        //constructor
+        public Floors()
+        {
+
+        }
+
+        public ArrayList GetAllFloors()
+        {
+            return FloorsTravelled;
+        }
+
+        public ArrayList AddFloor()
+        {
+            // add the floor pushed in to the Floors Travelled and keep the list sorted at all times. 
+            return FloorsTravelled;
+        }
+    }
+}
